b8fe03182e57a9fd4296832335449b7d6e186ea2
Eng. Brian Kiprono <89153335+bkiprono@users.noreply.github.com>
2025-02-09 23:24:26 +0300

    chore: update database configuration and enhance deployment script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,5 +1,7 @@
 sudo ln -s /etc/nginx/sites-available/invoices.zaitomedicalcentre.co.ke /etc/nginx/sites-enabled/invoices.zaitomedicalcentre.co.ke
 
+docker rm -f redis_zaito 
+
 docker compose up -d --build
 
 sudo systemctl reload nginx
